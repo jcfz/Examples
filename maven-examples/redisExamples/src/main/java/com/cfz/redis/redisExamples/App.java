39d54d0934283179a7d2b4c8e2f7da47e1b54b18
John Chan <chenfazhun@163.com>
2016-03-01 16:47:07 +0800

    test

--- maven-examples/redisExamples/src/main/java/com/cfz/redis/redisExamples/App.java
+++ maven-examples/redisExamples/src/main/java/com/cfz/redis/redisExamples/App.java
@@ -1,4 +1,6 @@
 package com.cfz.redis.redisExamples;
+import java.util.List;
+
 import redis.clients.jedis.Jedis;
 /**
  * Hello world!
@@ -11,7 +13,14 @@ public class App
         System.out.println( "Hello World!" );
         Jedis jedis = new Jedis("localhost");
         System.out.println("Connection to server sucessfully");
-        //check whether server is running or not
-        System.out.println("Server is running: "+jedis.ping());
+        //store data in redis list
+        jedis.lpush("tutorial-list", "Redis");
+        jedis.lpush("tutorial-list", "Mongodb");
+        jedis.lpush("tutorial-list", "Mysql");
+       // Get the stored data and print it
+       List<String> list = jedis.lrange("tutorial-list", 0 ,5);
+       for(int i=0; i<list.size(); i++) {
+         System.out.println("Stored string in redis:: "+list.get(i));
+       }
     }
 }
