607849b43530bbd2f4151836a02e8ca04dbede2f
John Chan <chenfazhun@163.com>
2016-03-01 15:44:10 +0800

    redis connect

--- maven-examples/redisExamples/src/main/java/com/cfz/redis/redisExamples/App.java
+++ maven-examples/redisExamples/src/main/java/com/cfz/redis/redisExamples/App.java
@@ -1,5 +1,5 @@
 package com.cfz.redis.redisExamples;
-
+import redis.clients.jedis.Jedis;
 /**
  * Hello world!
  *
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //check whether server is running or not
+        System.out.println("Server is running: "+jedis.ping());
     }
 }
