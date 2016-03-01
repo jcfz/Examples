51b7dbbeee518b8c43f60cf54953e7e81d49c82b
John Chan <chenfazhun@163.com>
2016-03-01 15:14:37 +0800

    commit maven first program

---
+++ maven-examples/redisExamples/src/main/java/com/cfz/redis/redisExamples/App.java
@@ -0,0 +1,13 @@
+package com.cfz.redis.redisExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
